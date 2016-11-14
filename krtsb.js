c886f176933f39da97e806acd6c17a8d461e9a5e
Andrii Voinov <andy.luminous@gmail.com>
2016-11-14 13:04:44 +0200

    karatsuba function, does not work with big numbers

--- krtsb.js
+++ krtsb.js
@@ -1,3 +1,28 @@
 function karatsubaFn(x, y) {
-    return 'Magic should happen here';
+    const base = 10;
+
+    let xLength = getExpLength(x),
+        yLength = getExpLength(y);
+
+    if (xLength <= 4 && yLength <= 4) {
+        return x * y;
+    }
+
+    let maxLength = Math.max(xLength, yLength),
+        n = Math.round(maxLength / 2);
+
+    let a = Math.floor(x / base ** n),
+        b = x % (base ** n),
+        c = Math.floor(y / base ** n),
+        d = y % (base ** n);
+
+    let ac = karatsubaFn(a,c),
+        bd = karatsubaFn(b,d),
+        adbc = karatsubaFn(a+b,c+d) - ac - bd;
+    
+    return ac * (base**(n*2)) + adbc * (base ** n) + bd; 
+}
+
+function getExpLength (x) {
+    return parseInt(x.toExponential().toString().split('e+')[1]) + 1;
 }
